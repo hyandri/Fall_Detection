8f24896fb5565bee429ed15fa42e2a18eb8a6da2
Bishika Chhetri <bishika.chhetri2077@gmail.com>
2025-11-13 08:20:29 +0545

    whatsapp message

--- backend/fall/fall/urls.py
+++ backend/fall/fall/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dashboard import views as dashboard_views
 
 urlpatterns = [
     path('', include('mainapp.urls')),
     path('admin/',admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('dashboard', include('dashboard.urls')),
+    path('dashboard', include('dashboard.urls')),   
+    path('admin/', admin.site.urls),
+    path('', dashboard_views.webcam_view, name='webcam'),
+
 
 ]
 urlpatterns=urlpatterns +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ model = YOLO("models/runs/detect/fall_detection_v22/weights/best.pt")
 
 # Run detection on one video 
 results = model.predict(
-    source="data/test/video (23).avi",  # path to a test video
+    source="runs/detect/predict/video (23).avi",  # path to a test video
     save=True,       # save output video
     conf=0.4,        # confidence threshold
     show=True       # shows video while processing
